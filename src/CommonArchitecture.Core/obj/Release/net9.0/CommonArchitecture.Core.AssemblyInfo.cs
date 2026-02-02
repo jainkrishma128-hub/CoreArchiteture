6b8df801d97ff0f40c694beae8417155ca4b216a
jainkrishma128-hub <jainkrishma128@gmail.com>
2026-02-01 17:55:02 -0800

    Latest Update

--- src/CommonArchitecture.Core/obj/Release/net9.0/CommonArchitecture.Core.AssemblyInfo.cs
+++ src/CommonArchitecture.Core/obj/Release/net9.0/CommonArchitecture.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonArchitecture.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36acf69f5efa687190ffdbf88377c74b8c5d3753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0951beafa7524a94bde5fc02404a7fa985bd3bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonArchitecture.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonArchitecture.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CommonArchitecture.Web/obj/Debug/net9.0/CommonArchitecture.Web.AssemblyInfo.cs
+++ src/CommonArchitecture.Web/obj/Debug/net9.0/CommonArchitecture.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36acf69f5efa687190ffdbf88377c74b8c5d3753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0951beafa7524a94bde5fc02404a7fa985bd3bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
